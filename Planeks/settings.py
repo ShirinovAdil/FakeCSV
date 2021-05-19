583c24ffc949d78828ef14ef42dfce9f4d767c97
Adil Shirinov <oxydoo1202@gmail.com>
2021-05-19 17:38:14 +0400

    media handled 2

--- Planeks/settings.py
+++ Planeks/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
-    'cloudinary_storage'
+    'cloudinary_storage',
 
     'celery',
     'django_celery_results',
